6df46c7ad237b03ffa8ec40d0f5b8b150398e3a1
Miguel <julianhernandez19@gmail.com>
2018-09-02 15:18:44 -0600

    [add] properties specific for each model

--- app/User.php
+++ app/User.php
@@ -9,13 +9,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USER_VERIFICATED = '1';
+    const USER_NOT_VERIFICATED = '0';
+
+    const USER_ADMIN = 'true';
+    const USER_REGULAR = 'false';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -24,6 +35,20 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token'
     ];
+
+    public function idVerificated() {
+        return $this->verified == User::USER_VERIFICATED;
+    }
+
+    public function idAdmin() {
+        return $this->admin == User::USER_ADMIN;
+    }
+
+    public static function generateVerificateToken() {
+        return str_random(40);
+    }
 }

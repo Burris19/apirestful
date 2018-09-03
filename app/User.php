7655601785c54424465d3049d0eaed25de154d22
Miguel <julianhernandez19@gmail.com>
2018-09-02 18:03:29 -0600

    [create] migrations and Factories for models

--- app/User.php
+++ app/User.php
@@ -15,6 +15,8 @@ class User extends Authenticatable
     const USER_ADMIN = 'true';
     const USER_REGULAR = 'false';
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Seller;
 use Faker\Generator as Faker;
 
 /*
@@ -19,5 +22,38 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'verified' => $verificado = $faker->randomElement([User::USER_VERIFICATED, User::USER_NOT_VERIFICATED]),
+        'verification_token' => $verificado == User::USER_VERIFICATED ? null : User::generateVerificateToken(),
+        'admin' => $faker->randomElement([User::USER_ADMIN, User::USER_REGULAR]),
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCT_AVAILABLE, Product::PRODUCT_NOT_AVAILABLE]),
+        'image' => $faker->randomElement(['default_1.png', 'dafault2.jpg']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(App\Transaction::class, function (Faker $faker) {
+
+    $vendedor = Seller::has('products')->get()->random(); 
+    $comprador = User::all()->except($vendedor->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->products->random()->id,
     ];
 });

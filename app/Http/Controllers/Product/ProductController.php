2405eb8e7ca1002b316f368746ab9c470b9ee8c3
Burris19 <julianhernandez19@gmail.com>
2018-09-15 17:01:03 -0600

    [add] implement show method for Product controller

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\ApiController;
 use App\Product;
-use Illuminate\Http\Request;
 
 class ProductController extends ApiController
 {
@@ -28,8 +27,8 @@ class ProductController extends ApiController
      */
     public function show(Product $product)
     {
-        //
+        return $this->showOne($product);
     }
 
-   
+
 }

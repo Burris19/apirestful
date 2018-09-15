c37133f3789c22a9d86402ae44e0e3cce7846da1
Burris19 <julianhernandez19@gmail.com>
2018-09-14 22:21:59 -0600

    [create] global scope for buyer model and inject model in controller buyer

---
+++ app/Scopes/BuyerScope.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miguelhernandez
+ * Date: 9/14/18
+ * Time: 10:16 PM
+ */
+
+namespace App\Scopes;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class BuyerScope implements Scope
+{
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $builder
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->has('transactions');
+    }
+}

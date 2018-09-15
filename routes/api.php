af73d17f4d2e141738feace0ef56f51b4f5a5eb4
Burris19 <julianhernandez19@gmail.com>
2018-09-15 17:29:38 -0600

    [add] route type resource for transaction controller

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,8 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
  */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+
 /**
  * Sellers
  */

a07d7969e90d3b2db0973188720a133da1ea7968
Burris19 <julianhernandez19@gmail.com>
2018-09-17 23:16:22 -0600

    [add] routes type resources by all category operations

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,15 @@ Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['onl
  */
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only']);
+
+Route::resource('categories.products', 'Category\CategoryProductController', ['only' => 'index']);
+
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => 'index']);
+
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => 'index']);
+
+
 /**
  * Products
  */

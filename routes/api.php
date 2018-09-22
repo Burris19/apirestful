4cae830dc344b29f7dbec1d3be25dcd6fbfc3c83
Burris19 <julianhernandez19@gmail.com>
2018-09-22 17:53:07 -0600

    [add] get all categories by specific product

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,8 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
  */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index']]);
+
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
 
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
